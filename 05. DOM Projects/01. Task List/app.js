b7fa9c5cbda337d311da871bca6924657ade2e9f
Malaxor <nogobaduk@gmail.com>
2018-09-12 18:00:45 -0700

    added filtering event listener

--- 05. DOM Projects/01. Task List/app.js
+++ 05. DOM Projects/01. Task List/app.js
@@ -6,6 +6,7 @@ const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 
+// add a task
 function addTask(e) {
 
     e.preventDefault();
@@ -30,7 +31,55 @@ function addTask(e) {
     // clear the input
     taskInput.value = '';
 }
-(function loadEventListeners() {
+// remove a task
+function removeTask(e) {
+    // event delegation
+    if(e.target.parentElement.classList.contains('delete-item')) {
 
+        if(confirm('Are you sure?')) {
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+}
+// remove ALL tasks
+function clearTasks(e) {
+    // easy way, but not faster
+    // taskList.innerHTML = '';
+    
+    // faster method
+    while(taskList.firstChild) {
+
+        taskList.removeChild(taskList.firstChild);
+    }
+}
+
+// filter tasks
+function filterTasks(e) {
+
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach(task => {
+
+        const item = task.firstChild.textContent;
+
+        if(item.toLowerCase().indexOf(text) != -1) {
+
+            task.style.display = 'block';
+        }
+        else {
+            
+            task.style.display = 'none';
+        }
+    });
+}
+
+(function loadEventListeners() {
+    // add task
     form.addEventListener('submit', addTask);
+    // remove task
+    taskList.addEventListener('click', removeTask);
+    // delete all tasks
+    clearBtn.addEventListener('click', clearTasks);
+    // filter tasks
+    filter.addEventListener('keyup', filterTasks);
 }());
