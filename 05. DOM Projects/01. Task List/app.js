c7d39680a40d7439adac92f29512f2f62263b064
Malaxor <nogobaduk@gmail.com>
2018-09-12 17:05:01 -0700

    add task item

---
+++ 05. DOM Projects/01. Task List/app.js
@@ -0,0 +1,40 @@
+
+// UI Variables
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+// Load all event listeners
+loadEventListeners();
+
+function loadEventListeners() {
+
+    form.addEventListener('submit', addTask);
+}
+
+function addTask(e) {
+
+    e.preventDefault();
+    if(!taskInput.value) {
+        alert('Add a task');
+    }
+    // create new li element
+    const li = document.createElement('li');
+    // add class
+    li.className = 'collection-item';
+    // create text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
+
+    // create new link element
+    const link = document.createElement('a');
+    link.className = 'delete-item secondary-content';
+    link.innterHTML = '<i class="fa fa-remove"></i>';
+    //append link to the li
+    li.appendChild(link);
+    // append li to the ul
+    taskList.appendChild(li);
+    // clear the input
+    taskInput.value = '';
+}
