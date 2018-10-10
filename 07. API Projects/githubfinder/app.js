68893d3b5bbfe911dcfa7560819aa29116075c9c
Malaxor <nogobaduk@gmail.com>
2018-10-09 17:44:16 -0700

    showing the alert

--- 07. API Projects/githubfinder/app.js
+++ 07. API Projects/githubfinder/app.js
@@ -14,15 +14,16 @@ searchUser.addEventListener('keyup', e => {
         .getUser(userText)
         .then(data => {
             if(data.profile.message === 'Not Found') {
-                // show alert
+                
+                ui.showAlert('User not found...', 'alert alert-danger');
             }
             else {
-                // show profile
+
                 ui.showProfile(data.profile);
             }
         });
     }
     else {
-        // clear profile
+        ui.clearProfile();
     }
 });
