3e270db2c9f034e5be33cb542985a94282ebbfa4
Malaxor <nogobaduk@gmail.com>
2018-10-10 18:07:37 -0700

    local storage constuctor working

--- 07. API Projects/openweather/app.js
+++ 07. API Projects/openweather/app.js
@@ -1,16 +1,39 @@
 
-const weather = new Weather('Detroit', 'Michigan');
+// Global Variables
+//=========================================================================
 const ui = new UI();
+const storage = new Storage();
 
+const weatherLocation = storage.getLocationData();
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
+//=========================================================================
+
+// Functions
+//============================================================================
 function getWeather() {
 
     weather
     .getWeather()
     .then(results => {
 
-        ui.paint(results);     
+        ui.paint(results);  
     })
     .catch(err => console.log(err));
 }
-
+// Event Listeners
+//============================================================================
 document.addEventListener('DOMContentLoaded', getWeather);
+
+const changeLocation = document.getElementById('w-change-btn');
+
+changeLocation.addEventListener('click', () => {
+
+    const city = document.getElementById('city').value;
+    const state = document.getElementById('state').value;
+
+    weather.changeLocation(city, state);
+    storage.setLocationData(city, state);
+    
+    getWeather();
+    $("#locModal").modal('hide');
+});
